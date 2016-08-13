5c766a7cebc5a43c44d5780bbbea01b76a1fbd72
h-mineta <h-mineta@0nyx.net>
2016-08-14 03:08:21 +0900

    [fix] service起動時に設定ファイルがない場合の対応追加

--- chinachu/services.sh
+++ chinachu/services.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-rm -f /var/run/chinachu-operator.pid
+if [ ! -f /usr/local/chinachu/conf/config.json ]; then
+	cat /usr/local/chinachu/config.sample.json > /usr/local/chinachu/conf/config.json
+fi
+
+rm -f /var/run/chinachu-operator.pid > /dev/null 2>&1
+rm -f /var/run/chinachu-wui.pid > /dev/null 2>&1
+
 /etc/init.d/chinachu-operator start
-rm -f /var/run/chinachu-wui.pid
 /etc/init.d/chinachu-wui start
+
 tail -f /dev/null
--- mirakurun/services.sh
+++ mirakurun/services.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if [ ! -f /usr/local/etc/mirakurun/channels.yml ]; then
+	cat /usr/local/share/mirakurun/channels.yml > /usr/local/etc/mirakurun/channels.yml
+fi
+if [ ! -f /usr/local/etc/mirakurun/tuners.yml ]; then
+	cat /usr/local/share/mirakurun/tuners.yml > /usr/local/etc/mirakurun/tuners.yml
+fi
+if [ ! -f /usr/local/etc/mirakurun/server.yml ]; then
+	cat /usr/local/share/mirakurun/server.yml > /usr/local/etc/mirakurun/server.yml
+fi
+
 pcscd
 /usr/bin/mirakurun start
 /usr/bin/mirakurun log server
