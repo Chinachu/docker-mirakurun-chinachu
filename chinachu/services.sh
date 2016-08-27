81f100006fcb1d36ed68471dff770feb31088e49
h-mineta <h-mineta@0nyx.net>
2016-08-27 14:36:42 +0900

    :heavy_plus_sign: mirakurun起動を待ってからchinachuが起動する方法へ変更(次のチャンネル同期用)

--- chinachu/services.sh
+++ chinachu/services.sh
@@ -6,6 +6,26 @@ fi
 rm -f /var/run/chinachu-operator.pid > /dev/null 2>&1
 rm -f /var/run/chinachu-wui.pid > /dev/null 2>&1
 
+countdown_max=300
+run_countdown=0
+while [ ${run_countdown} -le ${countdown_max} ]
+do
+	http_status=`curl -s container-mirakurun:40772/api/status -o /dev/null -w '%{http_code}'`
+	if [ "${http_status}" = "200" ]; then
+		run_countdown=0
+		echo "mirakurun is run complete!"
+		break
+	fi
+	run_countdown=`expr ${run_countdown} + 1`
+	echo "wait for mirakurun to run...(${run_countdown}sec)"
+	sleep 1
+done
+
+if [ ${run_countdown} -ge ${countdown_max} ]; then
+	echo "mirakurun is run failed" 1>&2
+	exit 1
+fi
+
 /etc/init.d/chinachu-operator start
 /etc/init.d/chinachu-wui start
 
